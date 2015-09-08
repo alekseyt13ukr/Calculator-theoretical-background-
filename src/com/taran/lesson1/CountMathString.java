0415c14cc68b5a0241a971043bffabebcec59944
alekseyt13ukr <alekseyt.ukr@mail.ru>
2015-09-08 16:54:57 +0300

    Finished counter of math string, no tests, bug with change sign

--- src/com/taran/lesson1/CountMathString.java
+++ src/com/taran/lesson1/CountMathString.java
@@ -1,5 +1,6 @@
 package com.taran.lesson1;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,16 +13,49 @@ public class CountMathString {
         this.listOfElements = listOfElements;
     }
 
-    public int countString() {
-        for (String currentElement: listOfElements) {
-            Pattern pattern = Pattern.compile("\\W");
-            Matcher matcher = pattern.matcher(currentElement);
+    public String countString() {
+        Pattern pattern = Pattern.compile("\\W");
+
+        for (int i = 0; i < listOfElements.size(); i++) {
+            System.out.println("i = " + i);
+            Matcher matcher = pattern.matcher(listOfElements.get(i));
+            System.out.println("current element in list = " + listOfElements.get(i));
 
             while (matcher.find()) {
-                System.out.println(matcher.group());
+                switch (matcher.group()) {
+                    case ("+") :
+                        int plus = Integer.parseInt(listOfElements.get(i - 2)) + Integer.parseInt(listOfElements.get(i - 1));
+                        replace((i - 2), plus);
+                        i = 0;
+                        break;
+                    case ("-") :
+                        int minus = Integer.parseInt(listOfElements.get(i - 2)) - Integer.parseInt(listOfElements.get(i - 1));
+                        replace((i - 2), minus);
+                        i = 0;
+                        break;
+                    case ("*") :
+                        int multiplace = Integer.parseInt(listOfElements.get(i - 2)) * Integer.parseInt(listOfElements.get(i - 1));
+                        replace((i - 2), multiplace);
+                        i = 0;
+                        break;
+                }
             }
         }
 
-        return 0;
+        String[] result = new String[listOfElements.size()];
+        result = listOfElements.toArray(result);
+
+        return result[0];
+    }
+
+    public void replace(int positionForReplace, int valuesForReplace) {
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Element for deleting = " + listOfElements.get(positionForReplace));
+            listOfElements.remove(positionForReplace);
+        }
+        String valuesForReplaceInString = String.valueOf(valuesForReplace);
+        listOfElements.add(positionForReplace, valuesForReplaceInString);
+        System.out.println("List of elements has view: " + listOfElements);
     }
 }
--- src/com/taran/lesson1/Main.java
+++ src/com/taran/lesson1/Main.java
@@ -3,10 +3,10 @@ package com.taran.lesson1;
 public class Main {
 
     public static void main(String[] args) {
-        Parser parser = new Parser("1+2*4-5+(5*111)");
+        Parser parser = new Parser("1+2*(5-3)+(5*111)");
         System.out.println("This is parsed string on elements: " + parser.separateString());
         System.out.println("This is reverse polish notation: " + parser.parse());
         CountMathString countMathString = new CountMathString(parser.parse());
-        countMathString.countString();
+        System.out.println("1+2*4-5+(5*111) = " + countMathString.countString());
     }
 }
